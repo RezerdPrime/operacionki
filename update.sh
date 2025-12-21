41adcd3b2d52d89eea9e5e9fa92a1d4d1ad6e4dd
rez <rez@vmo4ka.myguest.virtualbox.org>
2025-12-21 17:57:43 +1000

    Update project files

---
+++ update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "=========================================="
+echo "Github Update Script for C++/Cmake Project"
+echo "=========================================="
+
+REPO_PATH="/Desktop/operacionki"
+BRANCH="main"
+COMMIT_MESSAGE="Update project files"
+
+cd "$REPO_PATH"
+
+echo ""
+echo "1. Status checking..."
+git status
+
+echo ""
+echo "2. Acceptance of changes..."
+git add .
+
+echo ""
+echo "3. Commiting..."
+git commit -m "$COMMIT_MESSAGE"
+
+echo ""
+echo "4. Pull origin..."
+git pull origin "$BRANCH"
+
+echo ""
+echo "5. Push origin..."
+git push origin "$BRANCH"
+
+echo ""
+echo "============================="
+echo "Fin."
+echo "============================="
